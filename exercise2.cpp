9ce994ecf69069c70c554cdbd8a21de27b170440
hira <hiranaseem95@gmail.com>
2016-10-05 23:33:18 +0100

    Added exercise2

---
+++ exercise2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	int a,b;
+	cin >>a>>b;
+	
+	if((a%b)==0)
+	{
+		cout<<a<<" is a multiple of "<<b<<endl;
+	}
+	else
+	{
+		cout<<a<<" is NOT a multiple of "<<b<<endl;
+	}
+	
+
+return 0;
+	
+
+
+}
