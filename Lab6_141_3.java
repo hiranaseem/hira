7446a0b9a35284fb3009a255dc171b5690958159
hiranaseem <hiranaseem95@gmail.com>
2016-10-16 15:23:33 +0100

    For loop computation

--- Lab6_141_3.java
+++ Lab6_141_3.java
@@ -1,5 +1,7 @@
 package s1;
-/*Java program that uses a for-loop to compute:
+/*
+ *Year 1
+ *Java program that uses a for-loop to compute:
 - The sum of all even numbers between 2 and 100 (inclusive)
 - The sum of all squares between 1 and 100 (inclusive)
  * 
